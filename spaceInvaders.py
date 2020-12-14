f8dcfd42ddd5bed50ae4aa1e0093e960fc2b5175
Joris <jorisclement2@gmail.com>
2020-12-14 14:03:11 +0100

    header(explication du prg)

--- spaceInvaders.py
+++ spaceInvaders.py
@@ -1,1 +1,28 @@
-#Je suis une fleur
+#@Copyright: Joris CLEMENT, Corto Deschamps
+#Header
+"""
+ce programme permet de jouer dans une interface graphique à space invader
+vous avez 3 vie, le but et d'avoir de le meilleur score possible
+Auteur: Joris CLEMENT.
+date de réalisation: 22/12/2020
+
+
+Github:https://github.com/jorisclement/CS-Dev-TP3.git
+
+"""
+"""
+TODO -
+     -
+     -
+     -
+"""
+
+#Importation des bibliothéques
+
+
+
+#Fonction nécessaires au programme
+
+
+
+#Programme principal
